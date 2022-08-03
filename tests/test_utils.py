a064c23787e54495c7ed5a6da0a5df5d21587e12
ray <wuhaohsiang1992@gmail.com>
2022-08-03 09:38:01 +0800

    wip

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,7 +9,7 @@ test_datetime = datetime.datetime(year=2012, month=1, day=1, hour=1)
 
 class TestUtils(unittest.TestCase):
 
-    @patch("pytest_demo.utils.datetime.datetime")
+    @patch("src.utils.datetime.datetime")
     def test_get_current_hour1(self,mock_time):
         mock_time.now = Mock(return_value=test_datetime)
         hour = get_current_hour1()
